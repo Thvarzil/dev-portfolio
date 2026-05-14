f653fae499fd2c72a12275da123821c000fcd33f
thvarzil <louisblebohec@outlook.com>
2026-05-14 12:06:39 -0700

    file renames

--- backend/api/views.py
+++ backend/api/views.py
@@ -98,7 +98,7 @@ def portfolio_index(request):
         },
     }
 
-    return render(request, 'portfolio-3.html', context)
+    return render(request, 'index.html', context)
 
 
 @require_http_methods(["GET"])
